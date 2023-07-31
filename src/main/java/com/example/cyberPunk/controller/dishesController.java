c891af460443bc64eec17b925367abb8e83a39ba
ChenR-Y <10817154+chenr-y@user.noreply.gitee.com>
2023-07-31 22:29:02 +0800

    允许请求头不写

--- src/main/java/com/example/cyberPunk/controller/dishesController.java
+++ src/main/java/com/example/cyberPunk/controller/dishesController.java
@@ -2,6 +2,7 @@ package com.example.cyberPunk.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.cyberPunk.entity.Dishes;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/getDishes")
 public class dishesController {
 
