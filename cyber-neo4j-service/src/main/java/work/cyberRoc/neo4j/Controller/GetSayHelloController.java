c51819271dcfa7e0feccd0bfa3b5a441e4f4eb61
陈若愚 <10817154+chenr-y@user.noreply.gitee.com>
2023-12-14 04:58:42 +0800

    GATEWAY配置

---
+++ cyber-neo4j-service/src/main/java/work/cyberRoc/neo4j/Controller/GetSayHelloController.java
@@ -0,0 +1,18 @@
+package work.cyberRoc.neo4j.Controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GetSayHelloController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/getSayHello")
+    public String getSayHello(){
+        return  restTemplate.getForObject("http://cyber-system-service2/sayHello",String.class);
+    }
+}
